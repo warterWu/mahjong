f82d60f8d0efdea36bd168de5e0afb6371bebb28
Administrator <antcrazy@qq.com>
2017-04-22 17:49:53 +0800

    add event

--- src/main/java/com/mahjong/server/netty/model/EventEnum.java
+++ src/main/java/com/mahjong/server/netty/model/EventEnum.java
@@ -2,33 +2,62 @@ package com.mahjong.server.netty.model;
 
 /**
  * 操作类型枚举
- * @author 粱桂钊
- * @since 
- * <p>更新时间: 2016年7月31日  v0.1</p><p>版本内容: 创建</p>
+ * 
+ * @since
+ *        <p>
+ *        更新时间: 2016年7月31日 v0.1
+ *        </p>
+ *        <p>
+ *        版本内容: 创建
+ *        </p>
  */
 public enum EventEnum {
 	
-	/** 握手请求消息*/
+	/** 登录请求消息 */
     AUTH_REQ(1),
-    /** 握手响应消息*/
+	/** 登录响应消息 */
     AUTH_RESP(2),
     
-	/** 单聊请求消息 */
-    CHAT_REQ(3),
-    /** 单聊响应消息 */
-    CHAT_RESP(4),
+	/** 进入房间请求消息 */
+	ROOM_ENTER_REQ(3),
+	/** 进入房间响应消息 */
+	ROOM_ENTER_RESP(4),
     
-    /** 群聊请求消息 */
-    GROUP_CHAT_REQ(5),
-    /** 群聊响应消息 */
-    GROUP_CHAT_RESP(6),
+	/** 创建房间请求消息 */
+	CREATE_ROOM_REQ(5),
+	/** 创建房间响应消息 */
+	CREATE_ROOM_RESP(6),
     
-    /** 心跳请求消息*/
-    HEART_BEAT_REQ(7),
+	/** 解散房间请求消息 */
+	KILL_ROOM_REQ(7),
+	KILL_ROOM_RESP(8),
+	GRANT_RECORD_REQ(9),
     /** 心跳响应消息*/
-    HEART_BEAT_RESP(8)
-    
-    ;
+	GRANT_RECORD_RESP(10),
+	/**
+	 * 起牌请求
+	 */
+	GET_ONE_CARD_REQ(11),
+	/**
+	 * 起牌响应消息
+	 */
+	GET_ONE_CARD_RESP(12),
+	/**
+	 * 第一次发牌请求
+	 */
+	DEAL_CARD_START_REQ(13),
+	/**
+	 * 第一次发牌响应消息
+	 */
+	DEAL_CARD_START_RESP(14),
+	/**
+	 * 一次出牌请求
+	 */
+	DROP_ONE_CARD_REQ(15),
+	/**
+	 * 一次出牌请求响应
+	 */
+	DROP_ONE_CARD_RESP(16);
 
     private Integer value;
 
